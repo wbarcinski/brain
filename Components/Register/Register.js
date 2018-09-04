43be90e92d11f52607c6162c0156447f40f826e1
wbarcinski <wbarcinski@hotmail.com>
2018-09-04 13:37:48 -0500

    cont

--- Components/Register/Register.js
+++ Components/Register/Register.js
@@ -6,19 +6,19 @@ const Register = ({onRouteChange}) => {
 	return(
 
 		<main className="pa4 black-80">
-		  <form className="measure center">
+		  <div className="measure center">
 		    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
 		      <legend className="f1 fw6 ph0 mh0">Register</legend>
 		      <div className="mt3">
-		        <label className="db fw6 lh-copy f6" for="name">Name</label>
+		        <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
 		        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name"/>
 		      </div>
 		      <div className="mt3">
-		        <label className="db fw6 lh-copy f6" for="email-address">Email</label>
+		        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
 		        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
 		      </div>
 		      <div className="mv3">
-		        <label className="db fw6 lh-copy f6" for="password">Password</label>
+		        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
 		        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
 		      </div>
 		      {/*<label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox"/> Remember me</label>*/}
@@ -32,7 +32,7 @@ const Register = ({onRouteChange}) => {
 		      />
 		    </div>
 		    
-		  </form>
+		  </div>
 		</main>
 
 		
